ad66d18d3aa6086853cb2d9daa5ec085a80561aa
Emanuel_Fonse <efartecontato@gmail.com>
2024-08-10 01:06:55 -0300

    update: entities

---
+++ Domain/Entities/Evento.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Domain.ValueObject;
+
+namespace Domain.Entities
+{
+    public class Evento
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("eventoId")]
+        public int EventoId { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [Column("nome")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Data é obrigatória")]
+        [Column("data")]
+        public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "Descrição é obrigatória")]
+        [Column("descricao")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Capacidade é obrigatória")]
+        [Column("capacidade")]
+        public int Capacidade { get; set; }
+
+        [ForeignKey("Local")]
+        public int LocalId { get; set; }
+
+        public Local Local { get; set; }
+
+        public Evento(){ }
+
+        public Evento(int id, string nome, DateTime data, string descricao, int capacidade, int localId)
+        {
+            EventoId =id;
+            Name =nome;
+            Data =data;
+            Descricao =descricao;
+            Capacidade =capacidade;
+            LocalId =localId;
+        }
+    }
+}
---
+++ Domain/Entities/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("userId")]
+        public int UsuarioId { get; set; }
+
+        [Required(ErrorMessage = "Nome obrigatório")]
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Email obrigatório")]
+        [RegularExpression(@"^[a-zA-Z0-9+&-]+(?:\.[a-zA-Z0-9_+&-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,7}$", ErrorMessage = "Email inválido")]
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha obrigatória")]
+        [Column("senha")]
+        public string Senha { get; set; }
+
+        public Usuario() { }
+
+        public Usuario(int usuarioId, string nome, string email, string senha)
+        {
+            UsuarioId = usuarioId;
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+        }
+    }
+}
